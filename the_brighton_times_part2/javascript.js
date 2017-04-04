ad328bd6a2ecd3059ff7078cc2d6a74627ab6940
RDP <rdupaw@gmail.com>
2017-04-03 22:43:28 -0500

    Add javascript.js file to make menu animation actually animate

---
+++ the_brighton_times_part2/javascript.js
@@ -0,0 +1,12 @@
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
+
+menu.addEventListener('click', function(e) {
+	drawer.classList.toggle('open');
+	e.stopPropagation();
+});
+
+main.addEventListener('click', function() {
+	drawer.classList.remove('open');
+});
